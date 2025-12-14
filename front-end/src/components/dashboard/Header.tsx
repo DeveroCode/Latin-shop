d1ebdccfde626c84949bcf864bad710d92532791
DeveroCode <mtz.carlos.123.mz@code.com>
2025-12-13 21:39:19 -0800

    refactor: Remove unused state and commented code in Header component

--- front-end/src/components/dashboard/Header.tsx
+++ front-end/src/components/dashboard/Header.tsx
@@ -1,13 +1,10 @@
 import { useUser } from "@/hooks/user";
 import { Search } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { socketClient } from "../../socket/socket.ts";
-import Notification from "../ui/Notification.tsx";
 
 export default function Header() {
   const { data: user, isLoading } = useUser();
-  const [open, setOpen] = useState(false);
-  // Call the socket.io
   useEffect(() => {
     if (user?._id) {
       socketClient.connect(user._id);
@@ -28,29 +25,5 @@ export default function Header() {
           className="bg-gray-100/50 border border-gray-300 rounded-md py-2 px-4 pr-10 w-full focus:outline-none focus:ring-2 focus:ring-blue-900 placeholder:text-gray-600"
         />
       </form>
-      // <header className="py-5 bg-white border-b border-gray-200 flex items-center justify-between px-6">
-
-      //   {/* <div className="flex items-center gap-4">
-      //     <Notification id={user._id} open={open} setOpen={setOpen} />
-      //     <div className="flex items-center gap-2">
-      //       {user.image ? (
-      //         <img
-      //           src={user.image}
-      //           alt=""
-      //           className="w-10 h-10 object-cover rounded-full"
-      //         />
-      //       ) : (
-      //         <img
-      //           src="/user.webp"
-      //           alt=""
-      //           className="w-10 h-10 object-cover rounded-full"
-      //         />
-      //       )}
-      //       <span className="text-sm font-medium text-gray-700">
-      //         {user.name}
-      //       </span>
-      //     </div>
-      //   </div> */}
-      // </header>
     );
 }
