44b85a5c1e105e144d9fa625fdc6b012681345a8
DeveroCode <mtz.carlos.123.mz@code.com>
2025-12-30 22:31:37 -0800

    feat: update ImageProfileView layout and enhance profile image upload description

--- front-end/src/views/dashboard/settings/ImageProfileView.tsx
+++ front-end/src/views/dashboard/settings/ImageProfileView.tsx
@@ -1,15 +1,22 @@
 import Dropzone from "@/components/dashboard/Settings/Dropzone";
-import Subtitle from "@/components/ui/text/Subtitle";
-import Title from "@/components/ui/text/Title";
+import LabelText from "@/components/ui/text/LabelText";
+import Span from "@/components/ui/text/Span";
 
 export default function ImageProfileView() {
   return (
-    <div>
-      <Title>Profile Picture</Title>
-      <Subtitle>Upload or update your profile image</Subtitle>
-
+    <div className="flex justify-between gap-2 mt-5">
+      <div className="w-90 flex flex-col">
+        <LabelText id="profile_image">Update Profile Image</LabelText>
+        <Span>
+          Allows the user to upload or change their profile image. This image is
+          used to visually represent the account across the platform, including
+          the profile page, product listings, and interactions with other users.
+          The profile image can be updated at any time from the account
+          settings.
+        </Span>
+      </div>
 
       <Dropzone />
     </div>
-  )
+  );
 }
