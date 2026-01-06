8b55dd026b06322df9ff75bc84f54698c2b0e16b
DeveroCode <mtz.carlos.123.mz@code.com>
2026-01-05 20:36:21 -0800

    refactor: clean up SearchView component by improving variable naming and simplifying product search logic

--- front-end/src/views/Shop/SearchView.tsx
+++ front-end/src/views/Shop/SearchView.tsx
@@ -4,14 +4,22 @@ import KittyRain from "@/components/ui/KittyRain";
 import type { Product } from "@/types/index";
 import { useQueryClient } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+
 export default function SearchView() {
-  const { word } = useParams();
-  const QY = useQueryClient();
-  const producst = QY.getQueryData(["products-search", word]) as Product[];
+  const { word = "" } = useParams();
+  const queryClient = useQueryClient();
+
+  const products =
+    (queryClient.getQueryData([
+      "products-search",
+      word,
+    ]) as Product[]) ?? [];
+
+  const productsCount = products.length;
+
   const isKittySearch =
-    word?.toLocaleLowerCase().includes("kitty") ||
-    word?.toLocaleLowerCase().includes("hello") ||
-    word?.toLocaleLowerCase().includes("hello kitty");
+    word.toLowerCase().includes("kitty") ||
+    word.toLowerCase().includes("hello kitty");
 
   return (
     <div className="px-4 md:px-14 lg:px-20 xl:px-32 py-5">
@@ -21,10 +29,13 @@ export default function SearchView() {
         <legend className="font-montserrat capitalize text-xl">
           find product by {word}
         </legend>
-        <span className="text-gray-600 text-xs  capitalize">{`${
-          producst?.length > 1 ? "products" : "product"
-        } found ${producst?.length}`}</span>
+
+        <span className="text-gray-600 text-xs capitalize">
+          {productsCount}{" "}
+          {productsCount === 1 ? "product" : "products"} found
+        </span>
       </section>
+
       <section className="flex flex-col md:flex-row gap-10 py-10">
         <AsideCategories />
         <RenderSearchProducts />
